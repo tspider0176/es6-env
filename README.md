f571005ed1e507854dd40e4787a63a822bb3a966
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-11-22 04:14:09 +0900

    Add visjs sample

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # Hello vis.js
 
-## env
-webpack + babel
+## env(ES6)
+webpack + babel + eslint
 
-## run
+## init
 ```
 npm install
+```
+
+## eslint
+```
+$ eslint ./src/es2015/
 ```
 
 ## compile(ES6 -> ES5)
 
 ```
-`npm bin`/webpack
-open ./distribution/index.html
+$ `npm bin`/webpack
+$ open ./distribution/index.html
 ```
--- src/es2015/application.js
+++ src/es2015/application.js
@@ -1,15 +1,13 @@
-import Person from './person';
+import * as vis from "vis"
 
-class Friend extends Person{
-    constructor(name) {
-      super(name);
-    }
-    
-    callName() {
-      alert(this.name);
-    }
-}
-
-var friend = new Friend('Ryo');
-
-friend.callName();
+const container = document.getElementById("visualization");
+const data = [
+  {id: 1, content: "item 1", start: "2013-04-20"},
+  {id: 2, content: "item 2", start: "2013-04-14"},
+  {id: 3, content: "item 3", start: "2013-04-18"},
+  {id: 4, content: "item 4", start: "2013-04-16", end: "2013-04-19"},
+  {id: 5, content: "item 5", start: "2013-04-25"},
+  {id: 6, content: "item 6", start: "2013-04-27"}
+];
+const options = {};
+new vis.Timeline(container, data, options);
